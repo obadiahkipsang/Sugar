04bd1605c38dd7cf830c59a0560685eefe1d55e1
Obadiah Korir <korir400@gmail.com>
2018-05-01 11:04:48 +0300

    first commit

---
+++ app/src/main/java/com/example/chemisoft/sugar/Appointment_Activity.java
@@ -0,0 +1,18 @@
+package com.example.chemisoft.sugar;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+/**
+ * Created by CHEMISOFT on 4/16/2018.
+ */
+
+public class Appointment_Activity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
---
+++ app/src/main/java/com/example/chemisoft/sugar/ProgressActivity.java
@@ -0,0 +1,8 @@
+package com.example.chemisoft.sugar;
+
+/**
+ * Created by CHEMISOFT on 4/16/2018.
+ */
+
+public class ProgressActivity {
+}
